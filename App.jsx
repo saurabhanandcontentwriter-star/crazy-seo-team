257445760bbcec29983825ace0a7be7b4b1c77d3
sauravanand499-source <sauravanand499@gmail.com>
2026-05-03 12:27:30 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,14 +1,14 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import AdminLogin from "./AdminLogin";
 import AdminPage from "./AdminPage";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<AdminLogin />} />
         <Route path="/admin" element={<AdminPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
