ce56c5f77cdd16131f8d70eea2f1a9291febebe6
sauravanand499-source <sauravanand499@gmail.com>
2026-09-14 21:17:48 +0530

    restore original site visual theme

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,6 @@ import { createRoot } from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
 import App from "./App.tsx";
 import "./index.css";
-import "./threeD.css";
-import "./international.css";
 
 createRoot(document.getElementById("root")!).render(
   <HelmetProvider>
