4fc95870066c9fc17071bae89898ddd9e81a6793
sauravanand499-source <sauravanand499@gmail.com>
2026-09-18 09:04:01 +0530

    Expand Services page content for SEO

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -1,12 +1,14 @@
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import ServicesSection from "@/components/ServicesSection";
+import SEOContentBlock from "@/components/SEOContentBlock";
 
 const Services = () => (
   <div className="cst-public-page min-h-screen bg-background">
     <Navbar />
     <main className="pt-16">
       <ServicesSection />
+      <SEOContentBlock page="services" />
     </main>
     <Footer />
   </div>
