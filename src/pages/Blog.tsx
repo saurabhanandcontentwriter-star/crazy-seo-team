a62950ece9389f96c55b67ef7aeca076feec4b74
sauravanand499-source <sauravanand499@gmail.com>
2026-09-18 18:47:45 +0530

    Remove SEO content block from blog page

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -1,14 +1,12 @@
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import BlogSection from "@/components/BlogSection";
-import SEOContentBlock from "@/components/SEOContentBlock";
 
 const Blog = () => (
   <div className="cst-public-page min-h-screen bg-background">
     <Navbar />
     <main className="pt-16">
       <BlogSection />
-      <SEOContentBlock page="blog" />
     </main>
     <Footer />
   </div>
