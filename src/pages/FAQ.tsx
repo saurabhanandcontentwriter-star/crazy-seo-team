291a32371fb84e1c5dfaf094556c6dd4420b57bc
sauravanand499-source <sauravanand499@gmail.com>
2026-09-18 09:04:17 +0530

    Expand FAQ page content for SEO

--- src/pages/FAQ.tsx
+++ src/pages/FAQ.tsx
@@ -2,12 +2,14 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import FAQSection from "@/components/FAQSection";
 import WhatsAppButton from "@/components/WhatsAppButton";
+import SEOContentBlock from "@/components/SEOContentBlock";
 
 const FAQ = () => (
   <div className="min-h-screen bg-background">
     <Navbar />
     <div className="pt-16">
       <FAQSection />
+      <SEOContentBlock page="faq" />
     </div>
     <Footer />
     <WhatsAppButton />
