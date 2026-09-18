cb4e172a1a990348a50ff29cc749ab16ebeefcdb
sauravanand499-source <sauravanand499@gmail.com>
2026-09-18 09:03:58 +0530

    Expand About page content for SEO

--- src/pages/src/pages/Team.tsx
+++ src/pages/src/pages/Team.tsx
@@ -2,12 +2,14 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import AboutSection from "@/components/AboutSection";
 import WhatsAppButton from "@/components/WhatsAppButton";
+import SEOContentBlock from "@/components/SEOContentBlock";
 
 const About = () => (
   <div className="min-h-screen bg-background">
     <Navbar />
     <div className="pt-16">
       <AboutSection />
+      <SEOContentBlock page="about" />
     </div>
     <Footer />
     <WhatsAppButton />
