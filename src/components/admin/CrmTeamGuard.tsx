09ae531badb0c69e7f57d5318f0e76b38114ec35
Saurabh Anand <saurabhanandcontentwriter@gmail.com>
2026-09-24 20:35:03 +0530

    Allow all configured CRM admin accounts

--- src/components/admin/CrmTeamGuard.tsx
+++ src/components/admin/CrmTeamGuard.tsx
@@ -5,6 +5,8 @@ import { supabase } from "@/integrations/supabase/client";
 
 const CRM_ADMIN_EMAILS = new Set([
   "crazyseoteam@gmail.com",
+  "saurabhanandshahisarmera@gmail.com",
+  "saurabhanandcontentwriter@gmail.com",
   "sauravanand499@gmail.com",
 ]);
 
